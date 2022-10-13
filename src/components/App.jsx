ad3d4ac9f014c8059c2d4b8f2057fc6c1a18edbd
Oleksandr Mytrofanov <mitrofanov.322@gmail.com>
2022-10-13 09:49:06 +0300

    part 1
     add class, add countTotalFeedback and countPositiveFeedbackPercentage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,22 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, { Component } from 'react';
+
+export default class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+}
+
+countTotalFeedback = () => {
+  const { good, neutral, bad } = this.state;
+  const result = good + neutral + bad;
+  return result;
+};
+
+countPositiveFeedbackPercentage = () => {
+  const result = this.countTotalFeedback();
+  const { good } = this.state;
+  const percentage = (good * 100) / result;
+  return Math.round(percentage);
 };
